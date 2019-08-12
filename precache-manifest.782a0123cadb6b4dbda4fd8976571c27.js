89cfb2ab20849f7382912f365da5fa8e513fb4f4
rivan <rgdevrianza>
2019-08-12 22:35:07 +0700

    Updates

---
+++ precache-manifest.782a0123cadb6b4dbda4fd8976571c27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "996ee33746b6d8789176a28b15f260df",
+    "url": "/simple-fetch-search-react/index.html"
+  },
+  {
+    "revision": "064f8ca1423b820ecfab",
+    "url": "/simple-fetch-search-react/static/css/main.de0f850d.chunk.css"
+  },
+  {
+    "revision": "e6f182edb81f551cb0c1",
+    "url": "/simple-fetch-search-react/static/js/2.a4cf3094.chunk.js"
+  },
+  {
+    "revision": "064f8ca1423b820ecfab",
+    "url": "/simple-fetch-search-react/static/js/main.1f68d3bb.chunk.js"
+  },
+  {
+    "revision": "aa53b98098a2eab0b076",
+    "url": "/simple-fetch-search-react/static/js/runtime~main.8e714983.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simple-fetch-search-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
